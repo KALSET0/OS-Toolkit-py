bc686fd79a7bbbdf7d8375a1a75846590c98f272
KALSETO <jjcp101707@hotmail.com>
2026-06-27 17:11:17 -0500

    Enhance obtener_unidades_windows function with improved error handling and duplicate drive checks

--- files.py
+++ files.py
@@ -12,13 +12,19 @@ import platform
 
 
 def obtener_unidades_windows():
-    """Obtiene los puntos de montaje en Windows."""
+   """Obtiene los puntos de montaje en Windows."""
+    AMARILLO = "\033[33m"
+    ROJO = "\033[31m"
+    RESET = "\033[0m"
+    if platform.system() != "Windows":
+        print(f"obtener_unidades_windows():{ROJO} Error: Este método solo es compatible con Windows.{RESET}")
+        return []
+    
     drives = []
     
     try:
         # Obtener los puntos de montaje en Windows
         bitmask = __import__('ctypes').windll.kernel32.GetLogicalDrives()
-        
         # Iterar a través de las 26 letras del alfabeto para verificar qué los puntos de montaje están existentes
         for i in range(26):
             if bitmask & (1 << i):
@@ -27,10 +33,16 @@ def obtener_unidades_windows():
                 # Verificar si la letra ya está en la lista de drives antes de agregarla
                 if letra not in drives:
                     drives.append(letra)
-        return drives
-    except Exception:
+                else:
+                    print(f"obtener_unidades_windows():{AMARILLO} La unidad{RESET} {letra} {AMARILLO}encontrada duplicada, no se agregará a la lista.{RESET}")
+        if not drives:
+            print(f"obtener_unidades_windows():{AMARILLO} No se encontraron unidades montadas.{RESET}")
+        return drives if drives else []
+    except Exception as e:
+        print(f"obtener_unidades_windows():{ROJO} Error al obtener unidades:{RESET} {e}")
         __import__('traceback').print_exc()
         return []
+
 
 
 def obtener_unidades_mac():
